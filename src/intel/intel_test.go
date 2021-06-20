1d0d1f2a6c683312131bc13b3c700fb7e7d909ac
Moshiur Rahman <moshiur.rabby@gmail.com>
2021-06-20 17:55:04 +0800

    add more tests on intel package

--- src/intel/intel_test.go
+++ src/intel/intel_test.go
@@ -15,6 +15,20 @@ func Test_whereAmI(t *testing.T) {
 			So(c.OJ, ShouldEqual, "codeforces")
 			So(c.CurrentContestId, ShouldEqual, "1545")
 		})
+		Convey("valid-2", func() {
+			p := "/cf/src/1545"
+			c := whereAmI(p)
+			So(c, ShouldNotBeNil)
+			So(c.OJ, ShouldEqual, "codeforces")
+			So(c.CurrentContestId, ShouldEqual, "1545")
+		})
+		Convey("invalid", func() {
+			p := "/uva/src/problem-1245"
+			c := whereAmI(p)
+			So(c, ShouldNotBeNil)
+			So(c.OJ, ShouldNotEqual, "codeforces")
+			So(c.CurrentContestId, ShouldNotEqual, "1545")
+		})
 	})
 
 }
